a5797bbc0c91155766e4d51361bb99f7f679b418
Theforrest <79583255+Theforrest@users.noreply.github.com>
2023-02-05 17:04:21 +0100

    Cliente realizado
    Cliente  realizado con todos sus metodos, pasa todos los test sin problema

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,8 +1,6 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
-import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
-import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
-import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+
 
 public class MainApp {
 
